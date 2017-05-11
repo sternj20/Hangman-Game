11f63e56ef535779578586be7b01ea496f07d976
sternj20 <jessesternmusic@gmail.com>
2017-05-10 20:03:42 -0700

    added links to audio and logic so that it is played when an artist is correctly guessed, also logic to pause the previously played audio. added short bios that are rendered when guessed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -43,8 +43,17 @@ var guessedLettersHtml = document.getElementById('guessedLetters');
 var winsHTML = document.getElementById('wins');
 var winHTML = document.getElementById('winner');
 var instructionsHTML = document.getElementById('instructions');
+var audio = document.querySelectorAll('audio');
 var winTextHTML = document.getElementById('winnerText');
 var milesAudio = document.getElementById('milesAudio');
+var traneAudio = document.getElementById('traneAudio');
+var birdAudio = document.getElementById('birdAudio');
+var dukeAudio = document.getElementById('dukeAudio');
+var countAudio = document.getElementById('countAudio');
+var monkAudio = document.getElementById('monkAudio');
+var dizzyAudio = document.getElementById('dizzyAudio');
+var satchmoAudio = document.getElementById('satchmoAudio');
+var ellaAudio = document.getElementById('ellaAudio');
 
 //array of different words that the user will try to guess
 var words = ['miles', 'trane', 'bird', "duke", "count", "monk", "dizzy", "satchmo", "ella"];
@@ -101,7 +110,15 @@ document.onkeyup=function(event){
 		guessedLettersHtml.innerText=guessedLetters;
 	}
 	//tracking winner or loser
+
+
 	if (guessWord.join('') === randomWord && guessesLeft !== 0) {
+		// pausing audio of previous guess
+		for(i =0; i < audio.length; i++){
+			if(!audio[i].paused){
+				audio[i].pause();
+			}
+		}
 		//add one win to wins variable
 		wins++;
 		//update dom
@@ -110,40 +127,56 @@ document.onkeyup=function(event){
 			winHTML.src = "assets/images/milesmain.jpg";
 			winHTML.style.display = "block";
 			winTextHTML.innerText='Miles Davis';
-			instructionsHTML.innerText='Miles Dewey Davis III (May 26, 1926 – September 28, 1991) was an American jazz trumpeter, bandleader, and composer.';
+			instructionsHTML.innerText='Miles Dewey Davis III was an American jazz trumpeter, bandleader, and composer. He is among the most influential and acclaimed figures in the history of jazz and 20th century music.';
 			milesAudio.play();
 		} else if(randomWord ==='trane'){
 			winHTML.src = "assets/images/coltranemain.jpg";
 			winHTML.style.display = "block";
 			winTextHTML.innerText='John ("Trane") Coltrane';
+			instructionsHTML.innerText='John William Coltrane, also known as "Trane", was an American jazz saxophonist and composer. Working in the bebop and hard bop idioms early in his career, Coltrane helped pioneer the use of modes in jazz and was later at the forefront of free jazz.';
+			traneAudio.play();
 		}else if(randomWord ==='bird'){
 			winHTML.src = "assets/images/birdmain.jpg";
 			winHTML.style.display = "block";
 			winTextHTML.innerText= 'Charlie "Bird" Parker';
+			instructionsHTML.innerText='Charles "Charlie" Parker, Jr. (August 29, 1920 – March 12, 1955), also known as Yardbird and Bird, was an American jazz saxophonist and composer. Parker was a highly influential jazz soloist and a leading figure in the development of bebop, a form of jazz characterized by fast tempos, virtuosic technique and advanced harmonies.';
+			birdAudio.play();
 		}else if(randomWord ==='duke'){
 			winHTML.src = "assets/images/dukemain.jpg";
 			winHTML.style.display = "block";
 			winTextHTML.innerText= 'Duke Ellington';
+			instructionsHTML.innerText='Edward Kennedy "Duke" Ellington was an American composer, pianist, and bandleader of a jazz orchestra, which he led from 1923 until his death in a career spanning over fifty years.';
+			dukeAudio.play();
 		}else if(randomWord ==='count'){
 			winHTML.src = "assets/images/countmain.jpg";
 			winHTML.style.display = "block";
 			winTextHTML.innerText='Count Basie';
+			instructionsHTML.innerText='William James "Count" Basie (August 21, 1904 – April 26, 1984) was an American jazz pianist, organist, bandleader, and composer. Count Basie is considered one of the greatest bandleaders of all times. He was the arbiter of the big-band swing sound and his unique style of fusing blues and jazz established swing as a predominant music style.';
+			countAudio.play();
 		}else if(randomWord ==='monk'){
 			winHTML.src = "assets/images/monkmain.png";
 			winHTML.style.display = "block";
 			winTextHTML.innerText= 'Thelonious Monk';
+			instructionsHTML.innerText="Thelonious Sphere Monk (October 10, 1917[3] – February 17, 1982) was an American jazz pianist and composer. Monk had a unique improvisational style and made numerous contributions to the standard jazz repertoire. Monk is the second most-recorded jazz composer after Duke Ellington, which is particularly remarkable as Ellington composed more than a thousand pieces, whereas Monk wrote about 70.";
+			monkAudio.play();
 		}else if(randomWord==='dizzy'){
 			winHTML.src = "assets/images/dizzymain.jpg";
 			winHTML.style.display = "block";
 			winTextHTML.innerText='Dizzy Gillespie';
+			instructionsHTML.innerText='John Birks "Dizzy" Gillespie (October 21, 1917 – January 6, 1993) was an American jazz trumpeter, bandleader, composer, and singer, who  played with Charlie Parker and developed the music known as "bebop."';
+			dizzyAudio.play();
 		}else if(randomWord==='satchmo'){
 			winHTML.src = "assets/images/satchmomain.jpg";
 			winHTML.style.display = "block";
 			winTextHTML.innerText='Louis "Satchmo" Armstrong';
+			instructionsHTML.innerText='Louis Armstrong (August 4, 1901 – July 6, 1971), nicknamed Satchmo, Satch, or Pops was an American trumpeter, composer, singer and occasional actor who was one of the most influential figures in jazz.';
+			satchmoAudio.play();
 		}else if(randomWord==='ella'){
 			winHTML.src = "assets/images/ellamain.jpg";
 			winHTML.style.display = "block";
 			winTextHTML.innerText='Ella Fitzgerald';
+			instructionsHTML.innerText='Ella Jane Fitzgerald (April 25, 1917 – June 15, 1996) was an American jazz singer often referred to as the First Lady of Song, Queen of Jazz and Lady Ella, she was the most popular female jazz singer in the United States for more than half a century. In her lifetime, she won 13 Grammy awards and sold over 40 million albums.';
+			ellaAudio.play();
 		}
 		//generate new random word 
 		randomWord = randomWordGetter(words);
